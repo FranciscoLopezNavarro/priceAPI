045079fe16da4c22555ba6f7be1d64e72f0ff19f
Francisco López <francisco.lopez.navarro@kairosds.com>
2022-09-20 10:39:25 +0200

    chore: Create basic project archetype
    - Create basic project structure
    - Add H2 library to the pom

---
+++ src/test/java/com/kairos/gridtest/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kairos.gridtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
